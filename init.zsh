d8680cd666520f1db23ac58b672df40d4ecb042f
Branden Garrett <branden.garrett@gmail.com>
2020-01-19 09:49:49 -0500

    Fix homebrew nvm completion
    Add homebrew bash_completion.d/nvm to fpath

--- init.zsh
+++ init.zsh
@@ -5,4 +5,4 @@
 export NVM_DIR="$HOME/.nvm"
 
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && fpath+="/usr/local/opt/nvm/etc/bash_completion.d" # This loads nvm bash_completion
